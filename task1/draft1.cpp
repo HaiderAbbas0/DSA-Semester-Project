008551c8f78ed6a198432cfc8f91bf735dd85737
Hamd-Ul-Haq <i230081@isb.nu.edu.pk>
2024-11-24 14:29:03 +0500

    Task# 1 draft# 1 uploaded
    I have uploaded the first draft, for task# 1, this draft consists of:
    1- csv file reading.
    2- skeleton code for all the necessary classes and structs to be used in this task.

--- task1/draft1.cpp
+++ task1/draft1.cpp
@@ -1,1 +1,94 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
 
+struct Edge{
+
+    int weight;
+    Edge* next;
+
+    Edge(int weight1){
+        weight=weight1;
+        next=nullptr;
+    }
+};
+
+struct Node{
+
+    char value;
+    Edge* head;
+
+    Node(char value1){
+        value=value1;
+        head=nullptr;
+    }
+
+};
+
+class TrafficNetwork{
+
+    private:
+
+    Node* intersections;
+    int capacity;
+    int size;
+
+    public:
+
+    TrafficNetwork(int capacity1){
+        
+        capacity=capacity1;
+        size=0;
+        intersections=new Node[capacity];
+
+        for(int i=0;i<capacity;i++){
+            intersections[i].value=new Node('');
+        }
+
+    }
+
+    void addIntersection(){
+        
+        if(size>=capacity){
+            cout<<"Maximum capacity reached, can't add more intersections"<<endl;
+            return;
+        }     
+    }
+
+    void addRoad(){
+
+    }
+
+    void displayNetwork(){
+
+    }
+
+    ~TrafficNetwork(){
+        
+    }
+};
+
+
+int main(){
+
+    TrafficNetwork graph;
+
+    ifstream file("road_network.csv");
+    string line;
+
+    if(!file.is_open()){
+        cout<<"Error opening file!"<<endl;
+        return -1;
+    }
+
+
+    cout<<"Hello"<<endl;
+    while(getline(file,line)){
+        cout<<line<<endl;
+    }
+
+    file.close();
+
+    return 0;
+
+}
