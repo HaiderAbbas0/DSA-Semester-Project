dc2bb623088f4849d5ff72f57d2a952efcd21c26
Hamd-Ul-Haq <i230081@isb.nu.edu.pk>
2024-11-25 17:50:08 +0500

    Draft# 1 for task# 4 complete version
    The complete skeleton has been designed for the entire implementation.

--- task4/draft1.cpp
+++ task4/draft1.cpp
@@ -43,6 +43,22 @@ struct queueNode{
     }
 };
 
+struct linkedNode{
+   
+    int data;
+
+    linkedNode* next;
+   
+    linkedNode(int value){
+        data=value;
+        next=nullptr;
+    }
+};
+
+/***********************************
+* We will use Queue for bfs purposes
+***********************************/
+
 class Queue{
 
     private:
@@ -78,6 +94,95 @@ class Queue{
     void Display(){
     }
 };
+
+
+/*********************************
+* Using linked list for hash table
+*********************************/
+
+
+class LinkedList{
+   
+    private:
+   
+    linkedNode* head;
+    
+    public:
+    LinkedList(){
+        head=nullptr;
+    }
+
+    void insert(int value){
+        linkedNode* newNode=new linkedNode(value);
+       
+        if(head==nullptr){
+            head=newNode;
+        }
+       
+        else{
+            linkedNode* temp=head;
+           
+            while(temp->next!=nullptr){
+                temp=temp->next;
+            }
+            temp->next=newNode;
+        }
+    }
+
+    bool search(int value){
+ 
+        linkedNode* temp=head;
+        while(temp!=nullptr){
+           
+            if(temp->data==value){
+                return true;
+            }
+            temp=temp->next;
+        }
+        return false;
+    }
+
+    void display(){
+       
+        linkedNode* temp=head;
+       
+        while(temp!=nullptr){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+
+    bool search(int value){
+       
+        linkedNode* temp=head;
+
+        while(temp!=nullptr) {
+            if (temp->data == value) {
+                return true; 
+            }
+            temp = temp->next;
+        }
+        return false; 
+    }
+
+    void Del(){
+        linkedNode* temp=head;
+        head=head->next;
+        delete temp;
+
+        return;
+    }
+
+    ~LinkedList(){
+
+        while(head!=nullptr){
+            Del();
+        }
+    
+    }
+};
+
 
 class TrafficNetwork
 {
@@ -475,33 +580,64 @@ class Map
 
 class congestionMonitoring{
 
+    /*****************************************************************************
+    Logic:
+    
+    1- Our key is the path that a vehicle takes.
+    2- Our value is the vehicle count for that
+    specific path.
+    3- The capacity is the total number of roads.
+    4- The size is the current number of roads.
+    5- Congestion Level is an array, which will tell
+    us the level of congestion on each road.(we will
+    set the levels ourselves).
+    6- Congestion Marker is also an array, and it will
+    display the level of congestion for example moderate
+    low,high, we will set these values based on the congestion
+    level value set in congestion level array for example if my
+    congestion level for a specific road is in the range of 1-3
+    then I will mark the congestion marker for that specific road
+    as low, for 4-6 moderate, 7-9 high, anything above 10 as very
+    high.
+    7- we will link these two array together using their indexes,
+    each index represents a specific road, for example for road number
+    1 congestionLevel[1]=5, since it is in 4-6 range congestionMarker[1]="Moderate"  
+
+    ******************************************************************************/
+
 private:
     
     int capacity;
     int size;
-    int congestionTable;
-
+    int* congestionLevel;
+    string* congestionMarker;
+    LinkedList* congestionTable;
 
 public:
 
     congestionMonitoring(int capacity1){
         capacity=capacity1;
         size=0;
+        congestionTable=new LinkedList[capacity];
     }
 
     int hashFunction(const string& key){   
     }
 
-    void insert(const string& key, int value) {
+    void insert(const string& key, int value){
+        int index=hashFunction(key);
+        congestionTable[index].insert(value); 
     }
 
-    int search(const string& key) {
-    }
-
-    void remove(const string& key) {
+    int search(const string& key){
+        int index=hashFunction(key);
+        return congestionTable[index].search(index);
     }
 
     void display() {
+        for (int i = 0; i < capacity; i++) {
+            congestionTable[i].display();
+        }
     }
 };
 
